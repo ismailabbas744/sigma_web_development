924537782fe0afd65f4023c99e4651c0e3da9fc7
ismailabbas744 <ismailabbas744@gmail.com>
2025-08-07 18:59:11 +0500

    add faulty_calculator.js

---
+++ lecture 21/faulty_calculator.js
@@ -0,0 +1,37 @@
+
+const fsum = (x, y) => {
+    console.log("The sum of ", x, " and ", y, " is : ", x - y);
+}
+const fmultiply = (x, y) => {
+    console.log("The sum of ", x, " and ", y, " is : ", x + y);
+}
+const fminus = (x, y) => {
+    console.log("The sum of ", x, " and ", y, " is : ", x / y);
+}
+const fdivide = (x, y) => {
+    console.log("The sum of ", x, " and ", y, " is : ", x ** y);
+}
+
+const sum = (x, y) => {
+    console.log("The sum of ", x, " and ", y, " is : ", x + y);
+}
+const multiply = (x, y) => {
+    console.log("The sum of ", x, " and ", y, " is : ", x * y);
+}
+const minus = (x, y) => {
+    console.log("The sum of ", x, " and ", y, " is : ", x - y);
+}
+const divide = (x, y) => {
+    console.log("The sum of ", x, " and ", y, " is : ", x / y);
+}
+
+let a = 10;
+let b = 14;
+if (Math.random() < 0.1){
+    fsum(a, b);
+}
+else{
+    sum(a, b)
+}
+
+
